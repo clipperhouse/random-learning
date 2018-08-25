0f7e8a57600fc7f303c43b405da03ce8a2d29594
Matt Sherman <mwsherman@gmail.com>
2018-08-25 18:36:57 -0400

    Only use first video on a Khan topic

--- data/parse-and-persist.js
+++ data/parse-and-persist.js
@@ -5,62 +5,88 @@
         iterate(element, acc);
       });
     } else {
-      for (var property in obj) {
-        if (obj.hasOwnProperty(property)) {
+      let video = getFirstTopicVideo(obj);
+
+      if (video == null) {
+        for (let property in obj) {
+          if (!obj.hasOwnProperty(property)) {
+            continue;
+          }
+
           if (typeof obj[property] === "object") {
             iterate(obj[property], acc);
-          } else {
-            if (property === "content_kind" && obj[property] === "Video") {
-              if (obj["description"] < 16) {
-                continue;
-              }
-              if (
-                obj["image_url"] ===
-                "https://cdn.kastatic.org/googleusercontent/ZCdwTudJg6e6n-P2gsaUborP4izvMsGo71pvEVlX9dNYWcLXcP7VHkWpn2grt4TUP1KoJLQP9NswyHBuBLSFTBw"
-              ) {
-                continue;
-              }
-
-              const result = {
-                title: obj["title"],
-                description: obj["description"],
-                url: obj["ka_url"],
-                image_url: obj["image_url"],
-                favicon_url: "https://www.khanacademy.org/favicon.ico",
-                site: "Khan Academy",
-                site_class: "khan",
-                authors: obj["author_names"].join(", "),
-                duration: time(obj["duration"]) + " minutes"
-              };
-              acc.push(result);
-            }
           }
         }
+      } else {
+        acc.push(video);
       }
     }
   }
 
-  function isArray(o) {
-    return Object.prototype.toString.call(o) === "[object Array]";
+  function isArray(obj) {
+    return Object.prototype.toString.call(obj) === "[object Array]";
+  }
+
+  function getFirstTopicVideo(obj) {
+    if (!obj) {
+      return null;
+    }
+
+    if (obj["content_kind"] !== "Topic") {
+      return null;
+    }
+
+    let children = obj["children"];
+    if (!children || !isArray(children)) {
+      return null;
+    }
+
+    let first = children[0];
+    if (!first) {
+      return null;
+    }
+    if (first["content_kind"] !== "Video") {
+      return null;
+    }
+
+    if (
+      first["image_url"] ===
+      "https://cdn.kastatic.org/googleusercontent/ZCdwTudJg6e6n-P2gsaUborP4izvMsGo71pvEVlX9dNYWcLXcP7VHkWpn2grt4TUP1KoJLQP9NswyHBuBLSFTBw"
+    ) {
+      return null;
+    }
+
+    return {
+      title: first["title"],
+      description: first["description"],
+      url: first["ka_url"],
+      image_url: first["image_url"],
+      favicon_url: "https://www.khanacademy.org/favicon.ico",
+      site: "Khan Academy",
+      site_class: "khan",
+      authors: first["author_names"].join(", "),
+      duration: time(first["duration"]) + " minutes"
+    };
   }
 
   function time(secs) {
     return Math.round(secs / 60);
   }
 
-  const fs = require("fs");
-  const json = fs.readFileSync("topictree.f.json");
-  const topictree = JSON.parse(json);
+  let fs = require("fs");
+  let json = fs.readFileSync("topictree.f.json");
+  let topictree = JSON.parse(json);
 
-  var results = [];
+  let results = [];
   iterate(topictree, results);
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
-  const rand = getRandomInt(results.length - 1);
-  console.log("var thing = " + results[rand]);
+  console.log("found " + results.length);
+  let rand = getRandomInt(results.length - 1);
+  console.log(results[rand]);
 
   const jsonOut = JSON.stringify(results, null, 2);
   fs.writeFileSync("khan.json", jsonOut);
